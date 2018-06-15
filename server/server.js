1dad52e930a0d7fc9a8c7b16a3bc88ab39d67799
Steven <stevenryall812@gmail.com>
2018-06-15 17:40:58 +0100

    First pull for Football Live Updates

--- server/server.js
+++ server/server.js
@@ -8,16 +8,18 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}.`);
+  console.log(`Listening on port ${port}.`);
 });
 
 app.get("/latest_scores", async (req, res) => {
-    try {
-        const { rounds } = await fetch(
-            "https://raw.githubusercontent.com/openfootball/world-cup.json/master/2018/worldcup.json"
-        ).then(res => res.json());
-        res.send(rounds);
-    } catch (e) {
-        res.status(400).send(e);
-    }
+  try {
+    const { rounds } = await fetch(
+      "https://raw.githubusercontent.com/openfootball/world-cup.json/master/2018/worldcup.json"
+    ).then(res => res.json());
+    res.send(rounds);
+  } catch (e) {
+    res.status(400).send(e);
+  }
 });
+
+//this is code
