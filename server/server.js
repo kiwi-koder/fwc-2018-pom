1b653dfa0fde256204cee28b0545168ea1ffbc45
Joshua Rippon <joshua@pomerolpartners.com>
2018-06-19 14:17:19 +0100

    Sends back points from the group rounds.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const path = require("path");
-
+const { profiles } = require("./data/profiles.json");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,11 +13,65 @@ app.listen(port, () => {
 
 app.get("/latest_scores", async (req, res) => {
     try {
-        const { rounds } = await fetch(
-            "https://raw.githubusercontent.com/openfootball/world-cup.json/master/2018/worldcup.json"
+        const { groups } = await fetch(
+            "https://raw.githubusercontent.com/openfootball/world-cup.json/master/2018/worldcup.standings.json"
         ).then(res => res.json());
-        res.send(rounds);
+
+        let standings = [];
+        groups.forEach(group => {
+            standings = [...standings, ...group.standings];
+        });
+
+        const bottom16 = {
+            Russia: 1,
+            Egypt: 1,
+            "Saudi Arabia": 1,
+            Iran: 1,
+            Morocco: 1,
+            Australia: 1,
+            Iceland: 1,
+            Nigeria: 1,
+            Serbia: 1,
+            "Costa Rica": 1,
+            Sweden: 1,
+            "South Korea": 1,
+            Tunisia: 1,
+            Panama: 1,
+            Senegal: 1,
+            Japan: 1
+        };
+        const standingsWithTransformedPoints = standings.map(standing => {
+            if (bottom16[standing.team.name] === 1) {
+                const newPts = standing.pts * 2;
+                return { ...standing, newPts };
+            }
+            return { ...standing, newPts: standing.pts };
+        });
+        const profilesWithPoints = profiles.map(profile => {
+            const { topTeam, bottomTeam } = profile;
+            const topPoints = standingsWithTransformedPoints.find(
+                team => team.team.name === topTeam
+            ).newPts;
+            const bottomPoints = standingsWithTransformedPoints.find(
+                team => team.team.name === bottomTeam
+            ).newPts;
+            console.log(standingsWithTransformedPoints);
+            return {
+                ...profile,
+                totalPoints: topPoints + bottomPoints
+            };
+        });
+        // res.send(
+        //     standingsWithTransformedPoints.sort((a, b) => b.newPts - a.newPts)
+        // );
+
+        res.send(
+            profilesWithPoints.sort((a, b) => b.totalPoints - a.totalPoints)
+        );
     } catch (e) {
         res.status(400).send(e);
     }
 });
+
+//Top 16 (15) - Uruguay, Croatia, Belgium, Argentina, Switzerland, Portugal, Spain, Peru, Germany, Mexico, France, England, Brazil, Poland, Columbia
+//Bottom 16 - Sweden, Russia, Serbia, Iran, Iceland, Japan, Senegal, Tunisia, Panama, Morocco, Saudi Arabia, Costa Rica, South Korea, Egypt, Nigeria, Australia
