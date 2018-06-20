32cba6404a067d55b266474ed4ad437cd29dbb77
Joshua Rippon <joshua@pomerolpartners.com>
2018-06-20 09:33:21 +0100

    Changed build path

--- server/server.js
+++ server/server.js
@@ -114,9 +114,11 @@ app.get("/todays_matches", async (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static(path.join(__dirname, "..", "client", "build")));
     // Handle React routing, return all requests to React app
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "client/build", "index.html"));
+        res.sendFile(
+            path.join(__dirname, "..", "client", "build", "index.html")
+        );
     });
 }
